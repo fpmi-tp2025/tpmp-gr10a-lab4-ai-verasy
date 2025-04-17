a29f3ecdd4d6bd4f329f1f98326e85dd9693ebee
hallucinator <maksimenko.artem.2005gmail.com@mail.ru>
2025-04-17 15:54:41 +0300

    Create utils.h

---
+++ includes/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// Очистка буфера ввода
+void clear_input_buffer();
+
+// Проверка, является ли строка числом
+int is_numeric(const char *str);
+
+// Вывод заголовка меню
+void print_menu_header(const char *title);
+
+#endif
