93cc08d1cc04015301b6cb90b60ab84608e4543c
hallucinator <maksimenko.artem.2005gmail.com@mail.ru>
2025-04-17 15:47:41 +0300

    Update database.c

--- src/database.c
+++ src/database.c
@@ -5,8 +5,8 @@ sqlite3 *db;
 
 int init_database() {
     int rc = sqlite3_open("auction.db", &db);
-    if (rc) {
-        fprintf(stderr, "Не удается открыть БД: %s\n", sqlite3_errmsg(db));
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Ошибка открытия БД: %s\n", sqlite3_errmsg(db));
         return 0;
     }
     return 1;
@@ -16,12 +16,12 @@ void close_database() {
     sqlite3_close(db);
 }
 
-int execute_sql(const char *sql, int (*callback)(void*,int,char**,char**)) {
+int execute_sql(const char *sql, int (*callback)(void*, int, char**, char**)) {
     char *err_msg = 0;
     int rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
     
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL ошибка: %s\n", err_msg);
+        fprintf(stderr, "Ошибка SQL: %s\n", err_msg);
         sqlite3_free(err_msg);
         return 0;
     }
